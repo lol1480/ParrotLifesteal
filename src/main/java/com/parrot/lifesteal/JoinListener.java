eeb5e42c83080e2fb5db96af83a27ae06d3a7e18
lol1480 <ye5528856@gmail.com>
2026-04-28 19:06:24 +0300

    Update JoinListener.java

--- src/main/java/com/parrot/lifesteal/JoinListener.java
+++ src/main/java/com/parrot/lifesteal/JoinListener.java
@@ -1,14 +1,20 @@
-
 package com.parrot.lifesteal;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.*;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.entity.Player;
 
 public class JoinListener implements Listener {
+
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
-        HeartManager.set(p, HeartManager.get(p));
+
+        if (ReviveStorage.hasPending(p.getUniqueId())) {
+
+            HeartManager.set(p, 1);
+            ReviveStorage.restore(p);
+            ReviveStorage.remove(p.getUniqueId());
+        }
     }
 }
