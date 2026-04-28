7c5aeb0810b82b1c06c310e04899bc7ad423ebf5
lol1480 <ye5528856@gmail.com>
2026-04-28 19:04:22 +0300

    Update ReviveStorage.java

--- src/main/java/com/parrot/lifesteal/ReviveStorage.java
+++ src/main/java/com/parrot/lifesteal/ReviveStorage.java
@@ -3,12 +3,12 @@ package com.parrot.lifesteal;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.HashMap;
-import java.util.UUID;
+import java.util.*;
 
 public class ReviveStorage {
 
     private static final HashMap<UUID, ItemStack[]> stored = new HashMap<>();
+    private static final HashSet<UUID> pending = new HashSet<>();
 
     public static void save(Player p) {
         stored.put(p.getUniqueId(), p.getInventory().getContents());
@@ -19,5 +19,17 @@ public class ReviveStorage {
 
         p.getInventory().setContents(stored.get(p.getUniqueId()));
         stored.remove(p.getUniqueId());
+    }
+
+    public static void mark(UUID uuid) {
+        pending.add(uuid);
+    }
+
+    public static boolean hasPending(UUID uuid) {
+        return pending.contains(uuid);
+    }
+
+    public static void remove(UUID uuid) {
+        pending.remove(uuid);
     }
 }
