a68c355e2348a73a9eb1124bce0e93135cd9c79a
lol1480 <ye5528856@gmail.com>
2026-04-28 18:10:43 +0300

    Update InventoryManager.java

--- src/main/java/com/parrot/lifesteal/InventoryManager.java
+++ src/main/java/com/parrot/lifesteal/InventoryManager.java
@@ -1,1 +1,29 @@
+package com.parrot.lifesteal;
 
+import org.bukkit.inventory.ItemStack;
+import java.util.HashMap;
+import java.util.UUID;
+
+public class InventoryManager {
+
+    private static final HashMap<UUID, ItemStack[]> inventories = new HashMap<>();
+    private static final HashMap<UUID, ItemStack[]> armors = new HashMap<>();
+
+    public static void save(UUID uuid, ItemStack[] inv, ItemStack[] armor) {
+        inventories.put(uuid, inv);
+        armors.put(uuid, armor);
+    }
+
+    public static ItemStack[] getInv(UUID uuid) {
+        return inventories.get(uuid);
+    }
+
+    public static ItemStack[] getArmor(UUID uuid) {
+        return armors.get(uuid);
+    }
+
+    public static void remove(UUID uuid) {
+        inventories.remove(uuid);
+        armors.remove(uuid);
+    }
+}
