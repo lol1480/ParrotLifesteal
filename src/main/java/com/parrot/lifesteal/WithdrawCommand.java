31ef73a5efe25345c3c6554ca43952dd348b7b0b
lol1480 <ye5528856@gmail.com>
2026-04-28 18:53:05 +0300

    Update WithdrawCommand.java

--- src/main/java/com/parrot/lifesteal/WithdrawCommand.java
+++ src/main/java/com/parrot/lifesteal/WithdrawCommand.java
@@ -1,9 +1,8 @@
-
 package com.parrot.lifesteal;
 
 import org.bukkit.*;
-import org.bukkit.entity.Player;
 import org.bukkit.command.*;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
@@ -17,27 +16,48 @@ public class WithdrawCommand implements CommandExecutor {
 
         Player p = (Player) sender;
 
-        int hearts = HeartManager.get(p);
-
-        if (hearts <= 1) {
-            p.sendMessage(ChatColor.RED + "You need at least 2 hearts!");
+        if (args.length == 0) {
+            p.sendMessage(ChatColor.RED + "Usage: /withdraw <amount>");
             return true;
         }
 
-        HeartManager.set(p, hearts - 1);
+        int amount;
 
-        ItemStack heart = new ItemStack(Material.NETHER_STAR);
-        ItemMeta meta = heart.getItemMeta();
-        meta.setDisplayName(ChatColor.RED + "Heart");
+        try {
+            amount = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+            p.sendMessage(ChatColor.RED + "Invalid number!");
+            return true;
+        }
 
-        NamespacedKey key = new NamespacedKey(Main.getInstance(), "real_heart");
-        meta.getPersistentDataContainer().set(key, PersistentDataType.INTEGER, 1);
+        if (amount <= 0) {
+            p.sendMessage(ChatColor.RED + "Must be positive!");
+            return true;
+        }
 
-        heart.setItemMeta(meta);
+        int hearts = HeartManager.get(p);
 
-        p.getInventory().addItem(heart);
+        if (hearts - amount < 1) {
+            p.sendMessage(ChatColor.RED + "You must keep at least 1 heart!");
+            return true;
+        }
 
-        p.sendMessage(ChatColor.GREEN + "You withdrew 1 heart!");
+        HeartManager.set(p, hearts - amount);
+
+        for (int i = 0; i < amount; i++) {
+            ItemStack heart = new ItemStack(Material.NETHER_STAR);
+            ItemMeta meta = heart.getItemMeta();
+            meta.setDisplayName(ChatColor.RED + "Heart");
+
+            NamespacedKey key = new NamespacedKey(Main.getInstance(), "real_heart");
+            meta.getPersistentDataContainer().set(key, PersistentDataType.INTEGER, 1);
+
+            heart.setItemMeta(meta);
+            p.getInventory().addItem(heart);
+        }
+
+        p.sendMessage(ChatColor.GREEN + "Withdrew " + amount + " hearts!");
+
         return true;
     }
 }
